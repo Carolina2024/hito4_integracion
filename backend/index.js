2ca5fb4a1e756204cdc7fd7828b7cdd1ac05c581
Carolina2024 <carcastillos@gmail.com>
2024-12-11 22:44:50 -0300

    feat/listo para iniciar los pasos de render

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+// HITO 4 BACKEND PROYECTO FINAL
+//para el backend
+require("dotenv").config();
+/* para habilitar los cors */
+const cors = require("cors");
+// Importar express y se ejecuta para obtener un enrutador (app)
+const express = require("express");
+const app = express();
+const usersRoutes = require("./routes/usersRoutes");
+
+// Configuración de CORS para permitir solicitudes con credenciales
+const corsOptions = {
+  origin: "http://localhost:5173",  // El origen de tu frontend
+  credentials: true,
+  exposedHeaders:["Authorization"]// Permitir enviar cookies o encabezados de autorización
+};
+app.use(cors(corsOptions)); // se permite cors para todas las rutas
+/* parsear el cuerpo de la consulta */
+app.use(express.json());
+app.use("/publicaciones", usersRoutes);
+app.use(usersRoutes);
+
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
+
+//para ocupar .env
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
+});
+
+
+app.get("*", (req, res) => {
+  console.log("dirname", _dirname);
+  /* res.status(404).send("ruta no encontrada") */
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+module.exports = app; // Exportamos app para los test
---
+++ frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  build: {
+    outDir: "../backend/public", // Construir el frontend en la carpeta del backend para render
+  },
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000", // Proxy para evitar problemas de CORS para render
+    },
+  },
+});
