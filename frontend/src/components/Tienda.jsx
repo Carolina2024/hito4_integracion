13f84ea0239431e990a0eee64ebf7dbb6fcebb1c
Carolina2024 <carcastillos@gmail.com>
2024-12-15 04:33:44 -0300

    cambiosssssssss

--- frontend/src/components/Tienda.jsx
+++ frontend/src/components/Tienda.jsx
@@ -18,8 +18,7 @@ const Tienda = () => {
       try {
         const response = await axios.get(
           /*  `${import.meta.env.VITE_BASE_URL}/publicaciones` */
-          "https://hito4-integracion.onrender.com/publicaciones"
-        );
+          "https://hito4-integracion.onrender.com/publicaciones");
         /*  console.log("Password:", pgpassword); */
         setPublicaciones(response.data || []); // Actualiza el estado con las publicaciones obtenidas
         /* console.log("publicaciones: ", response.data); */
